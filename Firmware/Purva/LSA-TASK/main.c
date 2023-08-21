8ad5d0667c0d35cf446829fcc6ddda73b1e614eb
purviyeshi <129578264+purviyeshi@users.noreply.github.com>
2023-08-22 00:33:19 +0530

    Update main.c

--- Firmware/Purva/LSA-TASK/main.c
+++ Firmware/Purva/LSA-TASK/main.c
@@ -28,7 +28,8 @@ void app_main(void)
     if (err != ESP_OK) {
         // Handle error here
     }
-	
+    
+   
    //Characterize ADC at particular atten
     esp_adc_cal_characteristics_t *adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
     esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
@@ -41,17 +42,27 @@ void app_main(void)
         printf("Default");
     }
    
-    uint16_t raw_sensor_readings[NUM_SENSORS];          //       Read and store raw values from ADC channels
+    uint16_t raw_sensor_readings[NUM_SENSORS];          //       Read and store raw values from ADC channels       
     
+    
+    const adc1_channel_t sensor_channels[NUM_SENSORS] = {
+        ADC1_CHANNEL_4,
+        ADC1_CHANNEL_7,
+        ADC1_CHANNEL_6,
+        ADC1_CHANNEL_3,
+        ADC1_CHANNEL_0
+    };
+       
     while (1) {
         for (int i = 0; i < NUM_SENSORS; i++) {
-            raw_sensor_readings[i] = adc1_get_raw(ADC1_CHANNEL_0 + i);
-        }        
+            raw_sensor_readings[i] = adc1_get_raw(sensor_channels[i]);
+        }
+
+        // Display the updated array of raw ADC readings
         
         ESP_LOGI(TAG, "LSA_1: %d \t LSA_2: %d \t LSA_3: %d \t LSA_4: %d \t LSA_5: %d", raw_sensor_readings[0], raw_sensor_readings[1], raw_sensor_readings[2], raw_sensor_readings[3], raw_sensor_readings[4]);
         
-        vTaskDelay(10 / portTICK_PERIOD_MS);        //      delay of 10 ms after each log
-        
-    }  
-	
+        vTaskDelay(10 / portTICK_PERIOD_MS); // Add a delay if needed
+    }
+   
 }
