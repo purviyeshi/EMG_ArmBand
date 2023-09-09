3b6416acbb7ca6ed8342559d939ef888b196d442
purviyeshi <129578264+purviyeshi@users.noreply.github.com>
2023-09-09 09:14:31 +0530

    Update MAC_ADDRESS_ESP.c

--- Firmware/Purva/MAC_ADDRESS_ESP/main/MAC_ADDRESS_ESP.c
+++ Firmware/Purva/MAC_ADDRESS_ESP/main/MAC_ADDRESS_ESP.c
@@ -1,27 +1,13 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 
-void app_main() {
-    // Initialize NVS (Non-Volatile Storage) to read WiFi configuration
-    // ...
-
-    // Initialize the TCP/IP stack
-    // ...
-
-    // Initialize the event loop
-    // ...
-
-    // Print MAC address to serial console
+void app_main() 
+{
+    
     uint8_t mac[6];
     esp_read_mac(mac, ESP_MAC_WIFI_STA);
     printf("Transmitter MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);                        
-    
-    // Start WiFi
-    // ...
-
-    // Other code
-    // ...
 }
 
 
